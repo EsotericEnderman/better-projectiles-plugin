812e59f98a4e51698bfa4639b0289b87cf8a00d2
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-31 14:01:12 +0000

    Format file

--- src/main/java/dev/enderman/minecraft/plugins/projectiles/better/listener/ProjectileHitListener.kt
+++ src/main/java/dev/enderman/minecraft/plugins/projectiles/better/listener/ProjectileHitListener.kt
@@ -12,68 +12,45 @@ import org.bukkit.event.entity.ProjectileHitEvent
 import java.util.*
 import kotlin.math.min
 
-class ProjectileHitListener(
-  private val plugin: BetterProjectilesPlugin
-) : Listener {
+class ProjectileHitListener(private val plugin: BetterProjectilesPlugin) : Listener {
+
   @EventHandler
-  fun onProjectileHit(
-    event: ProjectileHitEvent
-  ) {
+  fun onProjectileHit(event: ProjectileHitEvent) {
     val projectile = event.entity
     val hitEntity = event.hitEntity
 
-    val projectileType = projectile.type.toString().lowercase(
-        Locale.getDefault()
-      )
+    val projectileType = projectile.type.toString().lowercase(Locale.getDefault())
 
-    Bukkit.getLogger().info(
-        "projectileType = $projectileType"
-      )
+    Bukkit.getLogger().info("projectileType = $projectileType")
 
-    if (hitEntity == null) {
-      return
-    } else if (hitEntity is Snowman) {
+    if (hitEntity == null) return
+
+    if (hitEntity is Snowman) {
       if (projectile is Snowball) {
         event.isCancelled = true
 
         val health = hitEntity.health
-        val maxHealthAttribute = checkNotNull(
-          hitEntity.getAttribute(
-            Attribute.GENERIC_MAX_HEALTH
-          )
-        )
+        val maxHealthAttribute = hitEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!
         val maxHealth = maxHealthAttribute.value
 
-        hitEntity.health = min(
-          health + maxHealth / 16.0, maxHealth
-        )
+        hitEntity.health = min(health + maxHealth / 16.0, maxHealth)
       }
     }
 
-    Bukkit.getLogger().info(
-        "hitEntity.type = " + hitEntity.type
-      )
+    Bukkit.getLogger().info("hitEntity.type = " + hitEntity.type)
 
     val configuration = plugin.config as YamlConfiguration
     val configurationSection = configuration.getConfigurationSection(
       "projectiles.$projectileType"
     )
 
-    if (configurationSection == null) {
-      return
-    }
+    if (configurationSection == null) return
 
-    val damage = configurationSection.getDouble(
-      "damage"
-    )
-    val knockback = configurationSection.getDouble(
-      "knockback"
-    )
+    val damage = configurationSection.getDouble("damage")
+    val knockback = configurationSection.getDouble("knockback")
 
     val source = projectile.shooter
 
-    hitEntity.velocity = projectile.velocity.normalize().multiply(
-        knockback / 20.0
-      )
+    hitEntity.velocity = projectile.velocity.normalize().multiply(knockback / 20.0)
   }
 }
