c553e6f3f7e54fc86709284bb6414b62256216ea
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-31 17:03:32 +0000

    Allow snow golems to be hurt!

--- src/main/java/dev/enderman/minecraft/plugins/projectiles/better/listener/SnowGolemRightClickListener.kt
+++ src/main/java/dev/enderman/minecraft/plugins/projectiles/better/listener/SnowGolemRightClickListener.kt
@@ -3,11 +3,14 @@ package dev.enderman.minecraft.plugins.projectiles.better.listener
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.attribute.Attribute
+import org.bukkit.entity.Player
 import org.bukkit.entity.Snowman
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractAtEntityEvent
 import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemStack
+import kotlin.math.max
 import kotlin.math.min
 
 class SnowGolemRightClickListener : Listener {
@@ -21,11 +24,11 @@ class SnowGolemRightClickListener : Listener {
 
     val health = entity.health
 
+    if (health <= 0) return
+
     val maxHealthAttributeInstance = entity.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!
 
     val maxHealth = maxHealthAttributeInstance.value
-
-    if (health == maxHealth) return
 
     val player = event.player
     val inventory = player.inventory
@@ -46,12 +49,29 @@ class SnowGolemRightClickListener : Listener {
         healthIncrease = when (heldItem.type) {
           Material.SNOW_BLOCK -> maxHealth / 2.0
           Material.SNOWBALL -> maxHealth / 8.0
-          else -> return
+          else -> {
+            onSnowGolemSnowTake(player, entity)
+            return
+          }
         }
       }
     }
 
     entity.health = min(health + healthIncrease, maxHealth)
     if (player.gameMode != GameMode.CREATIVE) heldItem.amount -= 1
+  }
+
+  private fun onSnowGolemSnowTake(player: Player, golem: Snowman) {
+    val health = golem.health
+
+    val maxHealthAttributeInstance = golem.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!
+    val maxHealth = maxHealthAttributeInstance.value
+
+    val isLargeHealthDecrease = player.isSneaking
+
+    val healthDecrease = if (isLargeHealthDecrease) maxHealth / 2.0 else maxHealth / 8.0
+
+    golem.health = max(health - healthDecrease, 0.0)
+    player.inventory.addItem(ItemStack(if (isLargeHealthDecrease) Material.SNOW_BLOCK else Material.SNOWBALL))
   }
 }
