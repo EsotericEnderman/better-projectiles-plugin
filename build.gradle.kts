ff97795b6e1c460fc5999fa204b22b0c8d7843cb
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-01 18:20:00 +0000

    Format build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
 plugins {
   `java-library`
   id("io.papermc.paperweight.userdev") version "1.7.1"
-  id("xyz.jpenilla.run-paper") version "2.3.0" // Adds runServer and runMojangMappedServer tasks for testing
-  id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.1.1" // Generates plugin.yml based on the Gradle config
+  id("xyz.jpenilla.run-paper") version "2.3.0"
+  id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.1.1"
   id("com.github.johnrengelman.shadow") version "8.1.1"
   kotlin("jvm")
 }
@@ -39,58 +39,33 @@ val javaVersion = 21
 val paperApiVersion = "1.21.3"
 
 java {
-  // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 11 installed for example.
   toolchain.languageVersion = JavaLanguageVersion.of(javaVersion)
 }
 
-// 1)
-// For >=1.20.5 when you don't care about supporting spigot
-// paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
-
-// 2)
-// For 1.20.4 or below, or when you care about supporting Spigot on >=1.20.5
-// Configure reobfJar to run when invoking the build task
-/*
-tasks.assemble {
-  dependsOn(tasks.reobfJar)
-}
- */
-
 dependencies {
   paperweight.paperDevBundle(paperApiVersion + "-R0.1-SNAPSHOT")
-  // paperweight.foliaDevBundle("1.21-R0.1-SNAPSHOT")
-  // paperweight.devBundle("com.example.paperfork", "1.21-R0.1-SNAPSHOT")
   implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks {
   compileJava {
-    // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
-    // See https://openjdk.java.net/jeps/247 for more information.
     options.release = javaVersion
   }
+
   javadoc {
-    options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    options.encoding = Charsets.UTF_8.name()
   }
+
   shadowJar {
     archiveClassifier.set("")
     configurations = listOf(project.configurations.runtimeClasspath.get())
   }
+
   build {
     dependsOn(shadowJar)
   }
-  // Only relevant when going with option 2 above
-  /*
-  reobfJar {
-    // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
-    // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
-    outputJar = layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar")
-  }
-   */
 }
 
-// Configure plugin.yml generation
-// - name, version, and description are inherited from the Gradle project.
 bukkitPluginYaml {
   main = project.group.toString() + ".minecraft.plugins.projectiles.better." + pascalcase(rootProject.name)
   name = "BetterProjectiles"
@@ -98,9 +73,11 @@ bukkitPluginYaml {
   authors.add(projectAuthor)
   apiVersion = paperApiVersion
 }
+
 repositories {
   mavenCentral()
 }
+
 kotlin {
   jvmToolchain(21)
 }
