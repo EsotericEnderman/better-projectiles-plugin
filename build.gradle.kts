3365064c3340c93d3d9cb2cc19af60f9555a11bb
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-01 18:52:02 +0000

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ fun pascalcase(kebabCaseString: String): String {
 val projectAuthor = "Esoteric Enderman"
 
 group = "dev.enderman"
-version = "0.1.0"
+version = "0.2.0"
 description = "A plugin that improves Minecraft's projectiles."
 
 val javaVersion = 21
